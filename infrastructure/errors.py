0e8a2063463eb0980f5129988bd29d11c905c9e8
santigandolfo <santiago.gandolfo@tiendanube.com>
2022-07-27 21:42:21 -0300

    Added possibility to send pngs
    Changed file error message

--- infrastructure/errors.py
+++ infrastructure/errors.py
@@ -4,4 +4,4 @@ from fastapi import HTTPException, status
 class FileTypeExceptionException(HTTPException):
     def __init__(self):
         self.status_code = status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
-        self.detail = 'The file should be an image'
+        self.detail = 'The image format is not supported'
