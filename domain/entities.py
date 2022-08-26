b60ff22a7ee6bf61f45414e251b20b406e80b94e
santigandolfo <santiago.gandolfo@tiendanube.com>
2022-08-26 01:01:39 -0300

    Created users CRUD

---
+++ domain/entities.py
@@ -0,0 +1,59 @@
+from bson import ObjectId
+from pydantic import BaseModel, Field, EmailStr
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class UserModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    email: EmailStr = Field(...)
+    role: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Jane Doe",
+                "email": "jdoe@example.com",
+                "role": "admin",
+            }
+        }
+
+
+class UpdateUserModel(BaseModel):
+    name: str | None
+    email: EmailStr | None
+    role: str | None
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Jane Doe",
+                "email": "jdoe@example.com",
+                "role": "admin",
+            }
+        }
--- infrastructure/settings.py
+++ infrastructure/settings.py
@@ -4,3 +4,4 @@ import os
 LOG_LEVEL = int(os.getenv('LOG_LEVEL', logging.INFO))
 MODEL_NAMES_LOCATION = os.getenv('MODEL_NAMES_LOCATION', './materials.txt')
 MODEL_LOCATION = os.getenv('MODEL_LOCATION', 'tensor_simplificado.pt')
+MONGO_URL = os.environ["MONGODB_URL"]
